84363a97f8a2440d0af2869193a935d33214244a
pucumt <weple@qq.com>
2017-05-01 16:18:16 +0800

    cancel the order in xingye

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var AdminEnrollTrain = require('./adminEnrollTrain.js'),
     TrainClass = require('./trainClass.js'),
-    schedule = require('node-schedule');
+    schedule = require('node-schedule'),
+    request = require('request');
 
 function scheduleCronstyle() {
     schedule.scheduleJob('0 * * * * *', function() {
@@ -11,14 +12,57 @@ function scheduleCronstyle() {
                     TrainClass.cancel(order.trainId)
                         .then(function(resultTrainClass) {
                             if (resultTrainClass && resultTrainClass.ok && resultTrainClass.nModified == 1) {
-                                AdminEnrollTrain.cancel(order._id);
-                                //send message back to swiftpass
+                                AdminEnrollTrain.cancel(order._id).then(function() {
+                                    //send message back to swiftpass
+                                    closeOrder(order._id);
+                                });
                             }
                         });
                 });
             }
         });
     });
-}
+};
+
+function toxml(sendObject) {
+    var keys = Object.getOwnPropertyNames(sendObject).sort();
+    var xmlContent = "<xml>";
+    keys.forEach(function(key) {
+        xmlContent = xmlContent + "<" + key + "><![CDATA[" + sendObject[key] + "]]></" + key + ">"
+    });
+    xmlContent = xmlContent + "</xml>";
+    return xmlContent;
+};
+
+function closeOrder(id) {
+    var sendObject = {
+        'mch_id': settings.mch_id,
+        'nonce_str': 'bfbeducation',
+        'out_trade_no': id,
+        'service': 'unified.trade.close'
+    };
+    var keys = Object.getOwnPropertyNames(sendObject).sort(),
+        strPay = "";
+    keys.forEach(function(key) {
+        var v = sendObject[key];
+        if ("sign" != key && "key" != key) {
+            strPay = strPay + key + "=" + v + "&";
+        }
+    });
+    strPay = strPay + "key=" + settings.key;
+    var md5 = crypto.createHash('md5'),
+        sign = md5.update(strPay).digest('hex').toUpperCase();
+    sendObject.sign = sign;
+    var data = toxml(sendObject);
+
+    request.post({
+            url: 'https://pay.swiftpass.cn:443/pay/gateway',
+            body: data
+        },
+        function(error, response, body) {
+            if (response.statusCode == 200) {} else {}
+        }
+    );
+};
 
 scheduleCronstyle();
--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 module.exports = {
     db: 'website',
     host: 'localhost',
-    port: '10105'
+    port: '10105',
+    mch_id: '7551000001', //101560037142
 };
